68049f2a4fbce0859ec10f982549570486d48c5d
jennylia <jennylian100@gmail.com>
2014-11-09 13:10:11 -0800

    just before breakin

--- SurvivalShooter/Assets/Scripts/Managers/GameOverManager.cs
+++ SurvivalShooter/Assets/Scripts/Managers/GameOverManager.cs
@@ -2,23 +2,38 @@
 
 public class GameOverManager : MonoBehaviour
 {
-    public PlayerHealth playerHealth;
-
-
-    Animator anim;
-
-
-    void Awake()
-    {
-        anim = GetComponent<Animator>();
-    }
-
-
-    void Update()
-    {
-        if (playerHealth.currentHealth <= 0)
-        {
-            anim.SetTrigger("GameOver");
-        }
-    }
+	public PlayerHealth playerHealth;       // Reference to the player's health.
+	public float restartDelay = 5f;         // Time to wait before restarting the level
+	
+	
+	Animator anim;                          // Reference to the animator component.
+	float restartTimer;                     // Timer to count up to restarting the level
+	
+	
+	void Awake ()
+	{
+		// Set up the reference.
+		anim = GetComponent <Animator> ();
+	}
+	
+	
+	void Update ()
+	{
+		// If the player has run out of health...
+		if(playerHealth.currentHealth <= 0)
+		{
+			// ... tell the animator the game is over.
+			anim.SetTrigger ("GameOver");
+			
+			// .. increment a timer to count up to restarting.
+			restartTimer += Time.deltaTime;
+			
+			// .. if it reaches the restart delay...
+			if(restartTimer >= restartDelay)
+			{
+				// .. then reload the currently loaded level.
+				Application.LoadLevel(Application.loadedLevel);
+			}
+		}
+	}
 }
